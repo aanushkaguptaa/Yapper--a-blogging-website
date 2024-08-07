3970a034c9dd90ce804bf833424df72f69618cc3
Anushka Gupta <anushkayol001@gmail.com>
2024-08-07 12:29:21 +0530

    added an alternate port

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.get('/', async (req, res)=>{
 app.use('/user', userRoute);
 app.use('/blog', blogRoute);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 })
