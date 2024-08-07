7e61071567ec82415995cc59ef1a083bbc5f624d
Anushka Gupta <anushkayol001@gmail.com>
2024-08-07 11:02:24 +0530

    connection changes to mongodb- successful locally

--- app.js
+++ app.js
@@ -7,17 +7,32 @@ const blogRoute= require('./routes/blog');
 const mongoose= require('mongoose');
 const cookieParser= require('cookie-parser');
 const blog= require('./models/blog');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.set("view engine", "ejs");
 
 const dbURI = process.env.MONGO_URL;
-
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true,   serverSelectionTimeoutMS: 50000  })
-  .then(() => console.log('Connected to MongoDB Atlas'))
-  .catch(err => {
-    console.error('Error connecting to MongoDB:', err);
-    process.exit(1);  // Exit the process if connection fails
-});
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(dbURI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 const path= require('path');
 const checkForCookies = require('./middlewares/authentication');
